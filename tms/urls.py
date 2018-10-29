0af37eda785c75ebb0938bf451430a7bd239a283
abc <developinghuman1015@gmail.com>
2018-10-29 21:25:40 +1000

    urls added

--- tms/urls.py
+++ tms/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from tms.views import CustomAuthToken
-from tms.views import CountryView
-from tms.views import AccountView
+
+from tms.views import AccountsView
+from tms.views import ClientsView
+from tms.views import CountriesView
+from tms.views import EarningsView
+from tms.views import ProjectsView
 
 urlpatterns = [
     path('login/', CustomAuthToken.as_view()),
-    path('countries', CountryView, basename='country'),
-    path('accounts', AccountView, basename='account')
+    path('accounts', AccountsView, basename='accounts'),
+    path('countries', CountriesView, basename='countries'),
+    path('clients', ClientsView, basename='clients'),
+    path('earnings', EarningsView, basename='earnings'),
+    path('projects', ProjectsView, basename='projects'),
 ]
